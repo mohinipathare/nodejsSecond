9bdf600b6cea1bf1079db11cfc8252b14bef0c79
mohinipathare <87515869+mohinipathare@users.noreply.github.com>
2023-03-28 16:29:10 +0530

    add routes to tasks1.js add products

--- task1.js
+++ task1.js
@@ -1,14 +1,19 @@
 //const http=require('http');
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-console.log('middleware 1');
-next();// Allows the request to continue to the next middleware in line
+app.use('/',(req,res,next)=>{
+    console.log('this always runs!');
+    next();// Allows the request to continue to the next middleware in line
 });
-app.use((req,res,next)=>
+app.use('/add-products',(req,res,next)=>
 {
-    console.log('middleware 2');
-    res.send('<h1>hello from expess!!!</h1>');
+    console.log('in middleware add-product');
+    res.send('<h1>In add-product page!!!</h1>');
+});
+app.use('/',(req,res,next)=>
+{
+    console.log('in another middleware');
+    res.send('<h1>In another middleware!!!</h1>');
 });
 //const server=http.createServer(app);
 app.listen(3000);
