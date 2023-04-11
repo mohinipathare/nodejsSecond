b89267745310248f9066bf6475e344034a4382e5
mohinipathare <87515869+mohinipathare@users.noreply.github.com>
2023-04-11 12:07:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,30 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 const errorController = require('./controllers/error');
-//const getContactUsContro=require('./controllers/contactUsSuccess');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const contactusRoute=require('./routes/ContactUs');
-const successRoute=require('./routes/success');
-
-
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(successRoute);
-app.use(contactusRoute);
+
 app.use(errorController.get404);
-//app.use(getContactUsContro.getContactUs);
 
 app.listen(3000);
