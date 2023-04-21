8a0379562767d72e66cbbb927f9a1408c4171952
mohinipathare <87515869+mohinipathare@users.noreply.github.com>
2023-04-21 11:42:40 +0530

    1.used sequelize

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize=require('./util/database');
+
 
 const app = express();
 
@@ -13,6 +15,10 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// db.execute("select * from products").then(result=>{
+// //console.log(result[0],result[1]);
+// }).catch();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,4 +27,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result=>{
+    //console.log(result);
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+});
